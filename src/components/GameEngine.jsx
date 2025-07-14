7ef32a22f73e26c4e19055e782908190efa973c8
vrouker <vrouker@gmail.com>
2025-07-14 14:06:24 -0400

    all achievements and unlocked achievements show on the account page and update after playing

--- src/components/GameEngine.jsx
+++ src/components/GameEngine.jsx
@@ -63,9 +63,9 @@ const GameEngine = ({
   //checks totalSeconds and then posts the correct achievement based on the db table
   function checkMilestoneAchievements(totalSeconds) {
     console.log(`line 35`, totalSeconds);
-    if (totalSeconds >= 10) unlockAchievement(1);
-    if (totalSeconds >= 15) unlockAchievement(2);
-    if (totalSeconds >= 20) unlockAchievement(3);
+    if (totalSeconds < 10) unlockAchievement(1);
+    if (totalSeconds < 15) unlockAchievement(2);
+    if (totalSeconds < 20) unlockAchievement(3);
     if (totalSeconds == 15.0) unlockAchievement(4);
   }
 
